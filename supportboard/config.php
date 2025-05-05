1311cf31ccf68170dedf2f03f18615bc0c008002
Christian Kloewer <kloewer@gmail.com>
2025-05-05 08:50:52 +0200

    initial commit

---
+++ supportboard/config.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * ==========================================================
+ * INITIAL CONFIGURATION FILE
+ * ==========================================================
+ *
+ * Insert here the information for the database connection and for other core settings.
+ *
+ */
+
+/* Plugin folder url */
+define('SB_URL', 'https://supportboard.tandempartner.org/supportboard');
+
+/* The name of the database */
+define('SB_DB_NAME', 'd04399c2');
+
+/* MySQL database username */
+define('SB_DB_USER', 'd04399c2');
+
+/* MySQL database password */
+define('SB_DB_PASSWORD', '9AEwcWHpNxyJkh8Fx5mm');
+
+/* MySQL hostname */
+define('SB_DB_HOST', 'localhost');
+
+/* MySQL port (optional) */
+define('SB_DB_PORT', '');
+
+/* [extra] */
+
+?>
